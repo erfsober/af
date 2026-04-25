556c6033e6195448d857184f6b6d3e38ed1b59b3
Erfan <erfaansabouri@gmail.com>
2026-04-25 21:29:28 +0330

    Add description to other debt

--- resources/views/exports/other-debt.blade.php
+++ resources/views/exports/other-debt.blade.php
@@ -2,6 +2,7 @@
     <thead>
     <tr>
         <th>پلاک</th>
+        <th>توضیحات</th>
         <th>مبلغ شارژ عقب افتاده</th>
         <th>مبلغ بدهی</th>
         <th>مبلغ شارژ ثابت ماهیانه</th>
@@ -14,6 +15,7 @@
         @endphp
         <tr>
             <td>{{ $other->plaque }}</td>
+            <td>{{ $other->description }}</td>
             <td>{{ $other->passed_due_date_amount }}</td>
             <td>{{ $other->otherDebts()->notPaid()->sum('amount') }}</td>
             <td>{{ $other->monthly_charge_amount }}</td>
