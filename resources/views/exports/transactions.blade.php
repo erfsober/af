a93960cb0faa1a707a6fd7ad34daa1f4d1cce2cc
Erfan <erfaansabouri@gmail.com>
2026-04-25 21:03:21 +0330

    Remove some columns in transactions export

--- resources/views/exports/transactions.blade.php
+++ resources/views/exports/transactions.blade.php
@@ -1,18 +1,12 @@
 <table>
     <thead>
     <tr>
-        <th>شناسه سیستم</th>
-        <th>نوع واحد</th>
         <th>موضوع</th>
         <th>پلاک</th>
-        <th>موبایل</th>
-        <th>نام کاربر</th>
         <th>شماره سفارش</th>
-        <th>وضعیت</th>
         <th>تاریخ و ساعت</th>
         <th>مبلغ اصلی</th>
         <th>مبلغ پرداختی</th>
-        <th>مبلغ زودتر از موعد</th>
         <th>مبلغ تخفیف</th>
         <th>مبلغ جریمه</th>
         <th>درصد تخیف</th>
@@ -22,18 +16,12 @@
     <tbody>
     @foreach($transactions as $transaction)
         <tr>
-            <td>{{ $transaction->id }}</td>
-            <td>{{ $transaction->tenant_or_other }}</td>
             <td>{{ $transaction->subject }}</td>
             <td>{{ $transaction?->tenant?->plaque }} {{ $transaction?->other?->plaque }}</td>
-            <td>{{ $transaction?->tenant?->phone_number }}</td>
-            <td>{{ $transaction?->tenant?->name }} - {{ $transaction?->tenant?->full_name }}</td>
             <td>{{ $transaction->ref_id }}</td>
-            <td>پرداخت شده</td>
             <td>{{ verta($transaction->paid_at)->format('Y-m-d H:i:s') }}</td>
             <td>{{ number_format($transaction->original_amount) }}</td>
             <td>{{ number_format($transaction->amount) }}</td>
-            <td>{{ number_format($transaction->zoodtarAmount()) }}</td>
             <td>{{ number_format($transaction->discountAmount()) }}</td>
             <td>{{ number_format($transaction->penaltyAmount()) }}</td>
             <td>{{ number_format($transaction->discountPercent()) }}</td>
